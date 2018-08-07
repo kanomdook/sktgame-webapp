96acad8a01cf56f02ff7921af7b6fe002b0107c1
kanomdook <kanung.2535@gmail.com>
2018-08-07 17:48:00 +0700

    [edit] module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, enableProdMode } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { AppRoutes } from './app.routing';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+
 
 enableProdMode();
 
@@ -15,7 +19,10 @@ enableProdMode();
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(AppRoutes)
+    HttpClientModule,
+    FormsModule,
+    RouterModule.forRoot(AppRoutes),
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
