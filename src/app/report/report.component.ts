fd37392a7b308684251c291a1e0d7900fa1e818b
kanomdook <kanung.2535@gmail.com>
2018-08-07 23:37:31 +0700

    [edit] report

--- src/app/report/report.component.ts
+++ src/app/report/report.component.ts
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./report.component.css']
 })
 export class ReportComponent implements OnInit {
-
+  page = 1;
+  page2 = 1;
   constructor() { }
 
   ngOnInit() {
