7c4e2b79b25096538aa2a1d97cfa96ccd2faf3b1
kanomdook <kanung.2535@gmail.com>
2018-08-08 17:26:39 +0700

    [edit] RegExp

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -10,6 +10,7 @@ export class DashboardComponent implements OnInit {
   page = 1;
   sportType = '';
   sportList: Array<any> = [];
+  sports: Array<any> = [];
   constructor(public api: Api) { }
 
   ngOnInit() {
@@ -20,9 +21,36 @@ export class DashboardComponent implements OnInit {
     try {
       const res: any = await this.api.get('/sports');
       this.sportList = res.data;
+      this.sports = res.data;
       console.log(this.sportList);
     } catch (error) {
       console.log(error);
+    }
+  }
+
+  changeSportType(e) {
+    if (e) {
+      const sports = this.sportList.filter(el => {
+        return el.sportType === e;
+      });
+      this.sports = sports;
+    } else {
+      this.sports = this.sportList;
+    }
+  }
+
+  search(e) {
+    this.sportType = '';
+    if (e) {
+      const sports = this.sportList.filter(el => {
+        const reg = new RegExp(e, 'g');
+        if (el.sportType.match(reg) || el.name.match(reg)) {
+          return el;
+        }
+      });
+      this.sports = sports;
+    } else {
+      this.sports = this.sportList;
     }
   }
 
